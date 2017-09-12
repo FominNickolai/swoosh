82c0298c55cfac91904b0930fb83a99400b10201
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-09-12 11:42:40 +0300

    Initial Commit

---
+++ Swoosh/Swoosh/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swoosh
+//
+//  Created by Fomin Mykola on 9/12/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
