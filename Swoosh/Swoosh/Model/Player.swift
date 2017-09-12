e413131bd90cd59d0bd6df49830a307f003188de
Mykola Fomin <giraffe.in.ua@gmail.com>
2017-09-12 15:44:14 +0300

    Added Player model. Added Logic for category selection.

---
+++ Swoosh/Swoosh/Model/Player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Fomin Mykola on 9/12/17.
+//  Copyright © 2017 Fomin Mykola. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevele: String?
+}
+
